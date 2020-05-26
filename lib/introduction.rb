5a871b713520f27703aeac6c5600f222b5b8d982
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-26 15:08:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,8 @@ def introduction (name)
   
 end
 
-def introduction_with_language
+def introduction_with_language (name, language)
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
